5c9b17bc7d8ca9b271ad391fe310d3883dfdebff
Nicolas J. Engler <nicojengler@gmail.com>
2019-04-24 15:26:53 -0300

    Add build files to repo

---
+++ docs/precache-manifest.bb66ac688c5c1c39855d0f1348f834a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff180122e00299804a86ffe739191eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29e0400e840bfb7e67eb",
+    "url": "/static/css/main.a77ebb92.chunk.css"
+  },
+  {
+    "revision": "3955ddb6fb5ebff97908",
+    "url": "/static/js/2.64d44950.chunk.js"
+  },
+  {
+    "revision": "29e0400e840bfb7e67eb",
+    "url": "/static/js/main.ed4c04b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "121f97e7c89e58b8a460f5a720fe5730",
+    "url": "/static/media/logo.121f97e7.svg"
+  }
+]);
